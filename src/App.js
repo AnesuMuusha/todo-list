c2632dd00cf57ce0b6b1bdeb2f6fafda18067f95
AnesuMuusha <anesumuusha@gmail.com>
2023-01-19 07:03:17 +0200

    comment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import ToDoItem from './Components/ToDoItem';
 import ToDoList from './Components/ToDoList';
 document.body.style = 'background: dodgerblue;'
+//add style on css page
 
 function App() {
   return (
